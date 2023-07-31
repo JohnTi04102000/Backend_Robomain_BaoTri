8df7e71324a61c8e6f85f454cd0eaa7cab2f0433
tri2312 <84215182+tri2312@users.noreply.github.com>
2023-07-31 20:18:47 +0700

    change random function

--- src/controller/workOrderController.js
+++ src/controller/workOrderController.js
@@ -1,103 +1,126 @@
 import pool from "../configs/connectDB";
 
 let getALLWorkOrders = async (req, res) => {
-    const [rows, fields] = await pool.execute("SELECT * FROM workOrders");
-  
-    return res.status(200).json({
-      message: "Get all workOrders successful",
-      data: rows,
+  const [rows, fields] = await pool.execute("SELECT * FROM workOrders");
+
+  return res.status(200).json({
+    message: "Get all workOrders successful",
+    data: rows,
+  });
+};
+
+let createNewWorkOrder = async (req, res) => {
+  let {
+    idAsset,
+    priority,
+    type,
+    status,
+    startDate,
+    completeDate,
+    description,
+    note,
+  } = req.body;
+
+  if (
+    !idAsset ||
+    !priority ||
+    !type ||
+    !status ||
+    !startDate ||
+    !completeDate ||
+    !description ||
+    !note
+  ) {
+    return res.status(404).json({
+      message: "WO failed",
     });
-  };
+  }
 
+  function generateRandomId() {
+    const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
 
-  let createNewWorkOrder = async (req, res) => {
-    let { idAsset, priority, type, status, startDate, completeDate, description, note } = req.body;
-  
-    if ( !idAsset || !priority || !type || !status || !startDate || !completeDate || !description || !note) {
-      return res.status(404).json({
-        message: "WO failed",
-      });
+    for (let i = 0; i < 6; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters[randomIndex];
     }
+    return result;
+  }
 
-    function generateRandomId() {
-      const randomString = Math.random().toString(6).substring(2); // Generate random hexadecimal string
-      return randomString;
-    }
-    
-    // Tạo một mã ngẫu nhiên
-    let id_WO = 'workOrder' + generateRandomId();
+  // Tạo một mã ngẫu nhiên
+  let id_WO = "workOrder" + generateRandomId();
 
-    let start = new Date(startDate);
-    let end = new Date(completeDate);
-  
-    await pool.execute("INSERT INTO workOrders values (?, ?, ?, ?, ?, ?, ?, ?, ?)", [
-      id_WO,
-      idAsset,
-      priority,
-      type,
-      status,
-      start,
-      end,
-      description,
-      note
-    ]);
-  
-    return res.status(200).json({
-      message: "Success",
+  let start = new Date(startDate);
+  let end = new Date(completeDate);
+
+  await pool.execute(
+    "INSERT INTO workOrders values (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+    [id_WO, idAsset, priority, type, status, start, end, description, note]
+  );
+
+  return res.status(200).json({
+    message: "Success",
+  });
+};
+
+let updateWorkOrder = async (req, res) => {
+  let {
+    id_WO,
+    idAsset,
+    priority,
+    type,
+    status,
+    startDate,
+    completeDate,
+    description,
+    note,
+  } = req.body;
+
+  if (
+    !id_WO ||
+    !idAsset ||
+    !priority ||
+    !type ||
+    !status ||
+    !startDate ||
+    !completeDate ||
+    !description ||
+    !note
+  ) {
+    return res.status(404).json({
+      message: "failed",
     });
-  };
+  }
 
+  let start = new Date(startDate);
+  let end = new Date(completeDate);
 
-  let updateWorkOrder = async (req, res) => {
-    let {id_WO, idAsset, priority, type, status, startDate, completeDate, description, note } = req.body;
-  
-    if (!id_WO || !idAsset || !priority || !type || !status || !startDate || !completeDate || !description || !note) {
-      return res.status(404).json({
-        message: "failed",
-      });
-    }
-  
-    let start = new Date(startDate);
-    let end = new Date(completeDate);
-  
-    const [user] = await pool.execute(
-      "UPDATE workOrders SET id_Asset = ?, priority = ?, type_WO = ?, status_WO = ?, startDate= ?, completeDate = ?, description_WO = ?, note= ? where id = ? ",
-      [
-      idAsset,
-      priority,
-      type,
-      status,
-      start,
-      end,
-      description,
-      note,
-      id_WO
-      ]
-    );
-    return res.status(200).json({
-      message: "Success",
+  const [user] = await pool.execute(
+    "UPDATE workOrders SET id_Asset = ?, priority = ?, type_WO = ?, status_WO = ?, startDate= ?, completeDate = ?, description_WO = ?, note= ? where id = ? ",
+    [idAsset, priority, type, status, start, end, description, note, id_WO]
+  );
+  return res.status(200).json({
+    message: "Success",
+  });
+};
+
+let deleteWorkOrder = async (req, res) => {
+  let id_WO = req.params.id;
+  if (!id_WO) {
+    return res.status(404).json({
+      message: "failed",
     });
-  };
+  }
+  await pool.execute("DELETE FROM workOrders WHERE id = ?", [id_WO]);
+  console.log("check: ", id_WO);
+  return res.status(200).json({
+    message: "Success",
+  });
+};
 
-
-  let deleteWorkOrder = async (req, res) => {
-    let id_WO = req.params.id;
-    if (!id_WO) {
-      return res.status(404).json({
-        message: "failed",
-      });
-    }
-    await pool.execute("DELETE FROM workOrders WHERE id = ?", [id_WO]);
-    console.log("check: ", id_WO);
-    return res.status(200).json({
-      message: "Success",
-    });
-  };
-
-  
 module.exports = {
-    getALLWorkOrders,
-    createNewWorkOrder,
-    updateWorkOrder,
-    deleteWorkOrder
-}
+  getALLWorkOrders,
+  createNewWorkOrder,
+  updateWorkOrder,
+  deleteWorkOrder,
+};
