9d890d0120ac7112a400e645e8f8dfe26997061d
tri2312 <84215182+tri2312@users.noreply.github.com>
2023-12-28 15:03:50 +0700

    Update api task

--- src/controller/taskController.js
+++ src/controller/taskController.js
@@ -10,100 +10,112 @@ let getALLTasks = async (req, res) => {
 };
 
 let createTask = async (req, res) => {
-  console.log('create-taskk ', req.body);
-  let { id_WO, description_Task, startDate, files, status_Task } = req.body;
+  try {
+    console.log("create-taskk ", req.body);
+    let { id_WO, description_Task, startDate, files, status_Task } = req.body;
 
-  if (!id_WO || !description_Task || !startDate || !files || !status_Task) {
-    return res.status(404).json({
-      message: "Wrong input data",
-    });
-  } else {
-    function generateRandomId() {
-      const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
-      let result = "";
+    if (!id_WO || !description_Task || !startDate || !files || !status_Task) {
+      return res.status(404).json({
+        message: "Wrong input data",
+      });
+    } else {
+      function generateRandomId() {
+        const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+        let result = "";
 
-      for (let i = 0; i < 6; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        result += characters[randomIndex];
+        for (let i = 0; i < 6; i++) {
+          const randomIndex = Math.floor(Math.random() * characters.length);
+          result += characters[randomIndex];
+        }
+        return result;
       }
-      return result;
-    }
 
-    // Tạo một mã ngẫu nhiên
-    let id = "task" + generateRandomId();
+      // Tạo một mã ngẫu nhiên
+      let id = "task" + generateRandomId();
 
-    let start = new Date(startDate);
+      let start = new Date(startDate);
 
-    try {
-      let result = await pool.execute(
-        "INSERT INTO tasks values (?, ?, ?, ?, ?, ?)",
-        [id, id_WO, description_Task, start, files, status_Task]
-      );
+      try {
+        let result = await pool.execute(
+          "INSERT INTO tasks values (?, ?, ?, ?, ?, ?)",
+          [id, id_WO, description_Task, start, files, status_Task]
+        );
 
-      if (result) {
-        console.log('result ', result);
-        return res.status(200).json({
-          message: "Create task successfully",
-        });
-      } else {
-        return res.status(404).json({
-          message: "Create task failed",
-        });
+        if (result) {
+          console.log("result ", result);
+          return res.status(200).json({
+            message: "Create task successfully",
+          });
+        } else {
+          return res.status(404).json({
+            message: "Create task failed",
+          });
+        }
+      } catch (err) {
+        console.log(err);
       }
-    } catch (err) {
-      console.log(err);
     }
+  } catch (err) {}
+  {
+    console.log(err);
   }
 };
 
 let getTaskById = async (req, res) => {
-  let id_WO = req.params.id;
-  if (!id_WO) {
-    return res.status(404).json({
-      message: `Please provide id work order`,
-    });
-  } else {
-    try {
-      const [rows, fields] = await pool.execute(
-        "Select * FROM tasks WHERE id_WO = ? ",
-        [id_WO]
-      );
-      //console.log("check: ", id_user);
-      if (rows.length > 0) {
-        return res.status(200).json({
-          message: `Get tasks of ${id_WO} successful`,
-          data: rows,
-        });
-      } else {
-        return res.status(404).json({
-          message: `Get workOrder of ${id_WO} failed`,
-        });
+  try {
+    let id_WO = req.params.id;
+    if (!id_WO) {
+      return res.status(404).json({
+        message: `Please provide id work order`,
+      });
+    } else {
+      try {
+        const [rows, fields] = await pool.execute(
+          "Select * FROM tasks WHERE id_WO = ? ",
+          [id_WO]
+        );
+        //console.log("check: ", id_user);
+        if (rows.length > 0) {
+          return res.status(200).json({
+            message: `Get tasks of ${id_WO} successful`,
+            data: rows,
+          });
+        } else {
+          return res.status(404).json({
+            message: `Get workOrder of ${id_WO} failed`,
+          });
+        }
+      } catch (e) {
+        console.log(e);
       }
-    } catch (e) {
-      console.log(e);
     }
+  } catch (err) {
+    console.log(err);
   }
 };
 
 let deleteTaskById = async (req, res) => {
-  let id_task = req.params.id;
-  if (!id_task) {
-    return res.status(404).json({
-      message: "Delete task failed",
-    });
+  try {
+    let id_task = req.params.id;
+    if (!id_task) {
+      return res.status(404).json({
+        message: "Delete task failed",
+      });
+    } else {
+      await pool.execute("DELETE FROM tasks WHERE id = ?", [id_task]);
+      console.log("check: ", id_task);
+      return res.status(200).json({
+        message: "Delete task successfully",
+      });
+    }
+  } catch (err) {
+    console.log(err);
   }
-  else{
-    await pool.execute("DELETE FROM tasks WHERE id = ?", [id_task]);
-    console.log("check: ", id_task);
-    return res.status(200).json({
-      message: "Delete task successfully",
-    });
-  }
-}
+};
 
 module.exports = {
   getALLTasks,
   createTask,
   getTaskById,
-  deleteTaskById
+  deleteTaskById,
 };
